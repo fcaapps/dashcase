e6d6e498783439ad78f666c7658b761ed30127e0
fcaapps <fca.apps@gmail.com>
2018-10-16 10:10:41 -0300

    Customização Admin - Iniciada

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Cliente
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+admin.site.site_header = 'DashCase - Painel de Administração'
+
+class ClienteAdmin(admin.ModelAdmin):
+     exclude = ('foto',)
+
+class UserAdmin(BaseUserAdmin):
+     list_filter = None
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.unregister(Group)
--- clientes/models.py
+++ clientes/models.py
@@ -10,3 +10,19 @@ class PermissoesClientes(models.Model):
         permissions = (
             ('clientes_permissoes', 'Permissão Global de Clientes'),
         )
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    nome_fantasia = models.CharField(max_length=255, null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    descricao = models.TextField(null=True, blank=True)
+    foto = models.ImageField(upload_to='clientes_fotos', null=True, blank=True)
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    alterado_em = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.nome + ' - ' + self.email
+
+
